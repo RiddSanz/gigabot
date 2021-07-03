f8ab0425b93e56c818ba978d7c3198a5dedd885a
ParidXploit <82357480+RiddSanz@users.noreply.github.com>
2021-07-03 19:15:30 +0700

    update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,13 +1,29 @@
-let fetch = require('node-fetch')
-let handler = async (m, { text }) => {
-  let res = await fetch(global.API('xteam', '/simsimi2', { kata: text }, 'APIKEY'))
-  let json = await res.json()
-  if (json.status) m.reply(json.result)
-  else throw json
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
+
+    if (!text) return conn.reply(m.chat, 'yaa ?' , m)
+
+  //await m.reply('Searching...')
+	axios.get(`https://lindow-api.herokuapp.com/api/simi?text=${text}&lang=id&apikey=LindowApi`).then ((res) => {
+	 	let hasil = `${res.data.response.text}`
+
+    conn.reply(m.chat, hasil, m)
+	})
 }
-handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
-handler.tags = ['fun']
-handler.command = /^((sim)?simi|simih)$/i
+handler.help = ['simi'].map(v => v + ' <text>')
+handler.tags = ['primbon']
+handler.command = /^(simi)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = true
 
 module.exports = handler
-
